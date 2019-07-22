be03836a2cb196945fea08e146d8b0431f662889
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-07-22 23:18:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(0, kittens.length -1);
-  return kittens.slice(0, kittens.length -1);
+ kittens =  kittens.slice(0, kittens.length -1);
+  return kittens;
 }
+
